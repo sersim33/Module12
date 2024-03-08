0da79514b88efffc9e87e42ec187c1b8be2eef79
sersim33 <sersim33@gmail.com>
2024-03-08 12:53:42 +0200

    com2

---
+++ task4.py
@@ -0,0 +1,26 @@
+import pickle
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = True
+    
+
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        self.filename = filename
+        self.contacts = contacts
+        
+    def save_to_file(self):
+        with open(self.filename, 'wb') as file:
+            pickle.dump(self.contacts, file)
+            
+    @staticmethod
+    def read_from_file(filename: str):
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
